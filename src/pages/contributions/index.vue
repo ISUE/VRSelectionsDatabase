512ce08ec04fca5a08133b3004792de8b199a64a
maslychm <maslychm@gmail.com>
2025-01-09 12:02:32 -0500

    fixed escaped slash in citation

--- src/pages/contributions/index.vue
+++ src/pages/contributions/index.vue
@@ -800,7 +800,7 @@ const citationText = ref<string>(
   `@inproceedings{maslych2024selectionsdatabase,
       author = {Maslych, Mykola and Yu, Difeng and Ghasemaghaei, Amirpouya and
         Hmaiti, Yahya and Martinez, Esteban Segarra and Simon, Dominic and
-        Taranta, Eugene Matthew and Bergstr{\"o}m, Joanna and LaViola Jr, Joseph J.},
+        Taranta, Eugene Matthew and Bergstr{\\"o}m, Joanna and LaViola Jr, Joseph J.},
       booktitle = {2024 IEEE International Symposium on Mixed and Augmented Reality (ISMAR)}, 
       title = {From Research to Practice: Survey and Taxonomy of Object Selection in Consumer VR Applications},
       url = {https://www.eecs.ucf.edu/isuelab/publications/pubs/MaslychISMAR2024.pdf},
